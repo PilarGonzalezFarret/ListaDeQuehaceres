2023b0591cd47454ccdb3f16cd357356a3a1c165
PilarGonzalezFarret <37448643+PilarGonzalezFarret@users.noreply.github.com>
2023-07-14 15:14:06 -0400

    Desarrollo

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Form = ({ handleSubmit, todo, editId, setTodo }) => {
+    return (
+        <form className="Form" onSubmit={handleSubmit}>
+            <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            />
+            <button type="submit"> {editId ? "Edit" : "Add"}</button>
+        </form>
+    );
+};
+
+export default Form;
